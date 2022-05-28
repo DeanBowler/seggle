0c1d839f3b10cbcf46c8241daba552d8f0fa6022
Dean Bowler <dean.bowler@gmail.com>
2022-05-28 11:14:54 +0100

    Reduce dictionary to 6 letter words

--- scripts/prepareDictionary.ts
+++ scripts/prepareDictionary.ts
@@ -4,8 +4,8 @@ import * as path from 'path';
 const result = fs.readFileSync(path.resolve(__dirname, './dictionary.json'), 'utf8');
 const words: string[] = JSON.parse(result);
 
-const MIN_LENGTH = 5;
-const MAX_LENGTH = 8;
+const MIN_LENGTH = 6;
+const MAX_LENGTH = 6;
 
 const stripped = words.filter(
   word => word.length >= MIN_LENGTH && word.length <= MAX_LENGTH,
