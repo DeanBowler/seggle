4b9852c7f360fe7cd9cdd30fcf936d9159b932b1
Dean Bowler <dean.bowler@gmail.com>
2023-07-23 22:59:29 +0100

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { ThemeProvider, Preflight, x } from '@xstyled/styled-components';
+import { DefaultTheme } from 'styled-components';
+import { Analytics } from '@vercel/analytics/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { theme } from '@/style/theme';
 import { GlobalStyles } from '@/style/globalStyles';
 import { Explainer } from '@/components/Explainer';
 import { Header } from '@/components/Header';
-import { Game } from '@/components/Game';
-import { DefaultTheme } from 'styled-components';
+import { Easy } from '@/screens/easy';
+
 import { SvgFilterDefinitions } from './components/SvgFilters';
 
 export function App() {
   return (
+    <>
+      <BrowserRouter>
     <ThemeProvider theme={theme as DefaultTheme}>
       <Preflight />
       <GlobalStyles />
@@ -17,9 +22,14 @@ export function App() {
       <Explainer />
       <x.div display="flex" flexDirection="column" h="full">
         <Header />
-        <Game />
+            <Routes>
+              <Route path="/" element={<Easy />} />
+            </Routes>
       </x.div>
       <SvgFilterDefinitions />
     </ThemeProvider>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
